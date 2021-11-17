531c20ae735e52a72bc12235acda96eb32666279
ryuinaa <93497990+ryuinaa@users.noreply.github.com>
2021-11-17 19:37:23 +0900

    21-11-17 pm 19:37
    zz

--- script.js
+++ script.js
@@ -6,26 +6,87 @@ $(function(){
         $(".submenu_wrap", this).stop().fadeOut(100);
     });
 
-//
-    $(".open").click(function(){
-        $(".fas").removeClass("fa-chevron-left").addClass("fa-chevron-right");
-        $(".slide_tab").animate({right:'-40px'}, 500);
-    });
-    $(".close").click(function(){
-        $(".fas").removeClass("fa-chevron-right").addClass("fa-chevron-left");
-        $(".slide_tab").animate({right:'-320px'}, 500);
-    });
+// 오른쪽에서 열리는 어사이드 메뉴
 
+$("#in-out").click(function(){
+    $(this).find("i:nth-of-type(1)").toggleClass("blind");
+    $("#side").toggleClass("onoff");
+})
 
-    $(function(){
+//이미지 슬라이드1
+        // setInterval(function(){
+        //     $(".slide").animate({left:"-1200"},1000,function(){
+        //         $(".slide li").eq(0).appendTo(".slide");
+        //         $(".slide").css("left",0);
+        //     });
+        //      console.log(1);
+        // },3000);
+//이미지 슬라이드2
         setInterval(function(){
-            $(".slide").animate({left:"-1200"},1000,function(){
-                $(".slide li").eq(0).appendTo(".slide");
-                $(".slide").css("left",0);
+            $(".adimg_slide").animate({left:"-1200"},1000,function(){
+                $(".adimg_slide li").eq(0).appendTo(".adimg_slide");
+                $(".adimg_slide").css("left",0);
             });
              console.log(1);
         },3000);
-    });
+
+// 새 이미지 슬라이더 실험
+$('.slider-1 > .page-btns > div').click(function(){
+    var $this = $(this);
+    var index = $this.index();
+    
+    $this.addClass('active');
+    $this.siblings('.active').removeClass('active');
+    
+    var $slider = $this.parent().parent();
+    
+    var $current = $slider.find(' > .slides > div.active');
+    
+    var $post = $slider.find(' > .slides > div').eq(index);
+    
+    $current.removeClass('active');
+    $post.addClass('active');
+});
+
+// 좌/우 버튼 추가 슬라이더
+$('.slider-1 > .side-btns > div').click(function(){
+    var $this = $(this);
+    var $slider = $this.closest('.slider-1');
+    
+    var index = $this.index();
+    var isLeft = index == 0;
+    
+    var $current = $slider.find(' > .page-btns > div.active');
+    var $post;
+    
+    if ( isLeft ){
+        $post = $current.prev();
+    }
+    else {
+        $post = $current.next();
+    };
+    
+    if ( $post.length == 0 ){
+        if ( isLeft ){
+            $post = $slider.find(' > .page-btns > div:last-child');
+        }
+        else {
+            $post = $slider.find(' > .page-btns > div:first-child');
+        }
+    };    
+    $post.click();
+});
+
+setInterval(function(){
+    $('.slider-1 > .side-btns > div').eq(1).click();
+}, 3000);
+
+
+
+
+
+
+
 
     //슬라이드 어사이드 메뉴 버튼
     // $(function(){
