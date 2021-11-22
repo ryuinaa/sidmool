8cecf73aa2d8552d962e41e857c3cdc4034042fd
wannit9 <espoir980921@gmail.com>
2021-11-22 15:53:38 +0900

    11.22 update
    1. aside tab slide 추가
    2. footer 수정
    3. pop up 추가
    4. scroll top 버튼 수정

--- script/script.js
+++ script/script.js
@@ -1,36 +1,60 @@
 $(function(){
-    $(".gnb .hover").mouseover(function(){
-        $(".submenu_wrap", this).stop().fadeIn(100);
-    });
-    $(".gnb .hover").mouseout(function(){
-        $(".submenu_wrap", this).stop().fadeOut(100);
-    });
-
-    // 오른쪽에서 열리는 어사이드 메뉴
-
-    $("#in-out").click(function(){
-        $(this).find("i:nth-of-type(1)").toggleClass("blind");
-        $("#side").toggleClass("onoff");
-    })
-
-    //이미지 슬라이드1
-            // setInterval(function(){
-            //     $(".slide").animate({left:"-1200"},1000,function(){
-            //         $(".slide li").eq(0).appendTo(".slide");
-            //         $(".slide").css("left",0);
-            //     });
-            //      console.log(1);
-            // },3000);
-    //이미지 슬라이드2
-    setInterval(function(){
-        $(".adimg_slide").animate({left:"-1200"},1000,function(){
-            $(".adimg_slide li").eq(0).appendTo(".adimg_slide");
-            $(".adimg_slide").css("left",0);
+    //gnb - 제품전체보기 hover : 서브메뉴
+        $(".gnb .hover").mouseover(function(){
+            $(".submenu_wrap", this).stop().fadeIn(150);
         });
-        console.log(1);
-    },3000);
-
-    // 새 이미지 슬라이더 실험
+        $(".gnb .hover").mouseout(function(){
+            $(".submenu_wrap", this).stop().fadeOut(200);
+        });
+    
+    
+    //어사이드 슬라이드 탭메뉴
+        // $(".tabopen").click(function(){
+        //     $(".side").animate({right:"0"},200);
+        // });
+        // $(".tabclose").click(function(){
+        //     $(".side").animate({right:"-300"},200);
+        // });
+        $("#in-out").click(function(){
+            $(this).find("i:nth-of-type(1)").toggleClass("blind");
+            $(".side").toggleClass("onoff");
+        });
+    
+    
+    
+    
+    //팝업창 광고 슬라이드
+        setInterval(function(){
+            $(".slide").animate({left:"-270"},500,function(){
+                $(".slide li").eq(0).appendTo(".slide");
+                $(".slide").css("left",0);
+            })
+        },3300);
+    
+    
+    
+    // 신상품 추천상품 Tab 메뉴
+        $(".tab_btn li").click(function(){
+            var ind = $(this).index();
+            $(".tab_btn li").removeClass("active"); // remove 하면
+            $(".tab_btn li").eq(ind).addClass("active"); // add 하기
+            $(".item2 ul").hide();
+            $(".item2 ul").eq(ind).show();
+        });
+    
+        
+    
+        // adimg 이미지슬라이드 영역
+        setInterval(function(){
+            $(".adimg_slide").animate({left:"-1200"},700,function(){
+                $(".adimg_slide li").eq(0).appendTo(".adimg_slide"); // appene'To' 지정해서 붙임
+                $(".adimg_slide").css("left",0);
+            })
+        },3300);
+    
+    
+    ////////////////////////////////////////
+    // contents wrap 이미지 슬라이더
     $('.slider-1 > .page-btns > div').click(function(){
         var $this = $(this);
         var index = $this.index();
@@ -47,8 +71,7 @@ $(function(){
         $current.removeClass('active');
         $post.addClass('active');
     });
-
-    // 좌/우 버튼 추가 슬라이더
+    // 좌/우 버튼
     $('.slider-1 > .side-btns > div').click(function(){
         var $this = $(this);
         var $slider = $this.closest('.slider-1');
@@ -76,8 +99,23 @@ $(function(){
         };    
         $post.click();
     });
-
     setInterval(function(){
         $('.slider-1 > .side-btns > div').eq(1).click();
-    }, 3000);
-});
+    }, 3500);
+    
+    
+    ////////////////////////////////////
+    // 팝업
+    $('.open').click(function(){
+        $('#popup').addClass('popup-open');
+      });
+      
+      $('.close').click(function(){
+        $('#popup').removeClass('popup-open');
+      });
+    
+    
+    
+    
+    }); // end
+    
