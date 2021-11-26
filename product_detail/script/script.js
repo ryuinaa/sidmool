38d0a472c44ed452deeb51924513cdb23762bdaa
jowoojeon <93627609+jowoojeon@users.noreply.github.com>
2021-11-26 10:06:29 +0900

    상품상세페이지-제이쿼리 추가 최종!~!
    상품상세페이지-제이쿼리 추가 최종!~!

--- product_detail/script/script.js
+++ product_detail/script/script.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
 });
 
 $(function(){
+  // gnb
   $(".gnb .hover").mouseover(function(){
       $(".submenu_wrap", this).stop().fadeIn(100);
   });
@@ -18,10 +19,51 @@ $(function(){
       $(".submenu_wrap", this).stop().fadeOut(100);
   });
 
-// 오른쪽에서 열리는 어사이드 메뉴
-
-  $("#in-out").click(function(){
-    $(this).find("i:nth-of-type(1)").toggleClass("blind");
-    $("#side").toggleClass("onoff");
+//어사이드 슬라이드 탭메뉴
+    // $(".tabopen").click(function(){
+    //     $(".side").animate({right:"0"},200);
+    // });
+    // $(".tabclose").click(function(){
+    //     $(".side").animate({right:"-300"},200);
+    // });
+    $("#in-out").click(function(){
+      $(this).find("i:nth-of-type(1)").toggleClass("blind");
+      $(".side").toggleClass("onoff");
   });
-})
+
+  
+  //팝업창 광고 슬라이드
+  setInterval(function(){
+  $(".slide").animate({left:"-270"},500,function(){
+      $(".slide li").eq(0).appendTo(".slide");
+      $(".slide").css("left",0);
+  })
+  },3300);
+
+  // adimg 이미지슬라이드 영역
+  setInterval(function(){
+    $(".adimg_slide").animate({left:"-1200"},700,function(){
+        $(".adimg_slide li").eq(0).appendTo(".adimg_slide"); // appene'To' 지정해서 붙임
+        $(".adimg_slide").css("left",0);
+    })
+  },3300);
+
+  // 팝업
+  $('.open').click(function(){
+    $('#popup').addClass('popup-open');
+  });
+
+  $('.close').click(function(){
+    $('#popup').removeClass('popup-open');
+  });
+
+
+  
+});
+
+
+
+
+
+
+  
